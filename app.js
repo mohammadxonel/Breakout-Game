d7497a817e884cffd56bd26ec64a1f7f1746fb48
moahammadxxali <ali_mohammad@t-online.de>
2023-04-10 04:44:09 +0200

    add js app and edits to grid

---
+++ app.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector('grid')
+const scoreDisplay = document.querySelector('#score')
+const blockWidth = 100
+const blockHeight = 20
+const ballDiameter = 20
+const boardWidth = 560
+const boardHeight = 300
+let xDirection = 2
+let yDirection = 2
+
+const userStart = [230, 10]
+let currentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+
+let timerId
+let score = 0
+
