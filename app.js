bac4f78d2a1d63748cce831df32035a51d347aae
moahammadxxali <ali_mohammad@t-online.de>
2023-04-10 21:20:03 +0200

    add js app edits to all the blocks

--- app.js
+++ app.js
@@ -28,3 +28,24 @@ class block {
     }
 }
 
+// all the blocks //
+
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+  ]
+
+  
