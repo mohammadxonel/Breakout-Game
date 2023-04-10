496c53444d5a8061d8a05603c8f0c8d133ac719f
moahammadxxali <ali_mohammad@t-online.de>
2023-04-10 21:30:39 +0200

    add js app edits to draw the blocks

--- app.js
+++ app.js
@@ -48,4 +48,19 @@ const blocks = [
     new Block(450, 210),
   ]
 
+  // draw the blocks //
+
+  function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+      const block = document.createElement('div')
+      block.classList.add('block')
+      block.style.left = blocks[i].bottomLeft[0] + 'px'  
+      block.style.bottom = blocks[i].bottomLeft[1] + 'px'  
+      grid.appendChild(block)
+      console.log(blocks[i].bottomLeft)
+    }
+  }
+
+  addBlocks()
+
   
